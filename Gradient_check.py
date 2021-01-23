4102943f492441b81399c53184c80e54f0b23f98
Victor Retamal <63514052+Retamal96@users.noreply.github.com>
2021-01-23 14:39:02 +0100

    Add files via upload

---
+++ Gradient_check.py
@@ -0,0 +1,80 @@
+
+import numpy as np
+
+def gradient_check_n_test_case():
+    np.random.seed(1)
+    x = np.random.randn(4,3)
+    y = np.array([1, 1, 0])
+    W1 = np.random.randn(5,4)
+    b1 = np.random.randn(5,1)
+    W2 = np.random.randn(3,5)
+    b2 = np.random.randn(3,1)
+    W3 = np.random.randn(1,3)
+    b3 = np.random.randn(1,1)
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+
+
+    return x, y, parameters
+
+def gradient_check_n(parameters, gradients, X, Y, epsilon = 1e-7):
+    """
+    Checks if backward_propagation_n computes correctly the gradient of the cost output by forward_propagation_n
+
+    Arguments:
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+    grad -- output of backward_propagation_n, contains gradients of the cost with respect to the parameters.
+    x -- input datapoint, of shape (input size, 1)
+    y -- true "label"
+    epsilon -- tiny shift to the input to compute approximated gradient with formula(1)
+
+    Returns:
+    difference -- difference (2) between the approximated gradient and the backward propagation gradient
+    """
+
+    # Set-up variables
+    parameters_values, _ = dictionary_to_vector(parameters)
+    grad = gradients_to_vector(gradients)
+    num_parameters = parameters_values.shape[0]
+    J_plus = np.zeros((num_parameters, 1))
+    J_minus = np.zeros((num_parameters, 1))
+    gradapprox = np.zeros((num_parameters, 1))
+
+    # Compute gradapprox
+    for i in range(num_parameters):
+
+        # Compute J_plus[i]. Inputs: "parameters_values, epsilon". Output = "J_plus[i]".
+        thetaplus = np.copy(parameters_values)
+        thetaplus[i][0] = thetaplus[i][0] + epsilon
+        J_plus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(thetaplus))
+
+
+        # Compute J_minus[i]. Inputs: "parameters_values, epsilon". Output = "J_minus[i]".
+        thetaminus = np.copy(parameters_values)
+        thetaminus[i][0] = thetaminus[i][0] - epsilon
+        J_minus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(thetaminus))
+
+        # Compute gradapprox[i]
+        gradapprox[i] = (J_plus[i] - J_minus[i]) / (2*epsilon)
+
+    # Compare gradapprox to backward propagation gradients by computing difference.
+    numerator = np.linalg.norm(gradapprox - grad )
+    denominator = np.linalg.norm(grad) + np.linalg.norm(gradapprox)
+    difference =  numerator / denominator                                          
+
+    if difference > 2e-7:
+        print ("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
+    else:
+        print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
+
+    return difference
+
+X, Y, parameters = gradient_check_n_test_case()
+
+cost, cache = forward_propagation_n(X, Y, parameters)
+gradients = backward_propagation_n(X, Y, cache)
+difference = gradient_check_n(parameters, gradients, X, Y)
